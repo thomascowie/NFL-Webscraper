6d05eebd79da47f30bb9cc465a3f7863983de7ca
Thomas <thomas.cowie12@gmail.com>
2022-10-24 00:19:16 -0400

    Team Matchups Create menu from Webscrape.

--- Team Matchups.py
+++ Team Matchups.py
@@ -1,6 +1,7 @@
-#Web Scraping for teams and their matchups.
+# Web Scraping for teams and their matchups.
 
 import requests
+from bs4 import BeautifulSoup
 
 # NFL Scores on ESPN
 URL = "https://www.espn.com/nfl/scoreboard"
@@ -8,18 +9,22 @@ page = requests.get(URL)  # Getting the Page
 
 soup = BeautifulSoup(page.content, "html.parser")  # Parsing as HTML
 
-#Get the team names
+# Get the team names
 teamNames = soup.find_all(
     "div", class_="ScoreCell__Team ScoreCell__Team--scoreboard flex-column pr2")
 
+# Fix the formatting to get rid of the html
+team_list = []
+for teamName in teamNames:
+    team = teamName.find(
+        "div", class_="ScoreCell__TeamName ScoreCell__TeamName--shortDisplayName truncate db")
+    team_list.append(team.text)
+
+# Print Menu
 print("Pick a team to get the schedule for.")
 iterator = 1
-for team in teamNames:
-    print(iterator + '. ' + team + " : " + team)
+for team in team_list:
+    print(iterator, '. ', team)
+    iterator = iterator + 1
 
-
-
-
-
-
-
+input("Team to get schedule from is: ")  # This needs fixing
